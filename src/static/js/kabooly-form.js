1f66b39ee2f0638d246f03941e9e00d0f55f53a4
David White <webdigga42@gmail.com>
2026-02-01 13:55:08 +0000

    Adding clickids to forms

--- src/static/js/kabooly-form.js
+++ src/static/js/kabooly-form.js
@@ -3,11 +3,15 @@
   if (!form) return;
 
   var iframe = document.createElement('iframe');
+
+  // Build iframe URL with theme and UTM parameters from parent page
   var baseUrl = 'https://crm.kabooly.com/embed/form/6';
   var params = [];
 
+
+  // Forward UTM parameters and click IDs from parent page URL to iframe
   var urlParams = new URLSearchParams(window.location.search);
-  ['utm_source', 'utm_campaign', 'utm_medium', 'utm_content', 'utm_term'].forEach(function(param) {
+  ['utm_source', 'utm_campaign', 'utm_medium', 'utm_content', 'utm_term', 'gclid', 'fbclid'].forEach(function(param) {
     var value = urlParams.get(param);
     if (value) params.push(param + '=' + encodeURIComponent(value));
   });
@@ -17,6 +21,7 @@
   iframe.style.border = 'none';
   iframe.style.minHeight = '400px';
   iframe.onload = function() {
+    // Auto-resize iframe based on content
     window.addEventListener('message', function(e) {
       if (e.data.formId === '6' && e.data.height) {
         iframe.style.height = e.data.height + 'px';
